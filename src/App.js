3f3cbeb4a7a262a38445699243c6024015495711
Tom Hart <kitsonian@gmail.com>
2022-02-25 15:55:28 -0500

    Adding status to buttons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState, useEffect } from 'react'
+
+const api = 'http://localhost:4000/api/'
+// const api = 'https://jsonplaceholder.typicode.com/users'
+
+// Second, a (create react app) react app which has two buttons: Fruit and veg. Clicking will async call the api and process the results to be presentable, replacing the previous results.
+// Consider state hooks vs reducer
 
 function App() {
+  
+  const [data, setData] = useState([])
+  const [filter, setFilter] = useState('getFruit')
+
+  
+  async function fetchAPI(){
+    try {
+      const fetchResult = await fetch(api + filter)
+      const json = await fetchResult.json()
+      setData(json)
+    } catch {
+      console.error('Unable to retrieve produce')
+    }
+  }
+
+  useEffect(() => {
+    fetchAPI()
+  }, [filter])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <button disabled={filter === 'getFruit'} onClick={() => setFilter('getFruit')}>Fruit</button>
+        <button disabled={filter === 'getVeg'} onClick={() => setFilter('getVeg')}>Veg</button>
       </header>
+      <div className='results'>
+        <ul className='list'>
+          {data.map((fruit, i) => <li key={i}>{fruit.name}</li>)}
+        </ul>
+      </div>
     </div>
   );
 }
